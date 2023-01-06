2457b036a8208949e105afcf3299a17edae302b0
Cristian <cristianadancastejon@gmail.com>
2023-01-06 20:36:30 +0100

    footer

--- components.tsx
+++ components.tsx
@@ -233,12 +233,12 @@ function Footer(props: { author?: string }) {
     <footer class="mt-20 pb-16 lt-sm:pb-8 lt-sm:mt-16">
       <p class="flex items-center gap-2.5 text-gray-400/800 dark:text-gray-500/800 text-sm">
         <span>
-          Powered by{" "}
+          Hecho por{" "}
           <a
             class="inline-flex items-center gap-1 underline hover:text-gray-800 dark:hover:text-gray-200 transition-colors"
-            href="https://deno.land/x/blog"
+            href="https://salteadorneo.dev/"
           >
-            Deno Blog
+            salteadorneo
           </a>
         </span>
         <a
